d0191f8363859b680b591d0d27fa6d119c8c517a
Percival Aceron <perci.aceron@gmail.com>
2024-07-20 17:37:37 +0800

    done with auth and users endpoint

--- backend/routes/api/usersRouter.js
+++ backend/routes/api/usersRouter.js
@@ -1,36 +1,25 @@
 import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
 import {
-  signupUser,
-  loginUser,
-  logoutUser,
-  refreshTokens,
   getCurrentUsers,
   updateAvatar,
+  updateUserInfo,
+  deleteAvatar,
   verifyEmail,
-  resendVerifyEmail,
-} from "../../controllers/authController.js";
+  resendVerifyEmail
+} from "../../controllers/usersController.js";
 import { authenticateToken } from "../../middleWares/authenticateToken.js";
 import { upload } from "../../middlewares/upload.js";
 
 const router = express.Router();
 
-router.post("/auth/register", ctrlWrapper(signupUser));
-
-router.post("/auth/login", ctrlWrapper(loginUser));
-
-router.get("/auth/logout", authenticateToken, ctrlWrapper(logoutUser));
-
-router.post("/auth/refresh", ctrlWrapper(refreshTokens));
-
 router.get("/current", authenticateToken, ctrlWrapper(getCurrentUsers));
 
-router.patch(
-  "/avatars",
-  authenticateToken,
-  upload.single("avatar"),
-  ctrlWrapper(updateAvatar)
-);
+router.patch("/info", authenticateToken, ctrlWrapper(updateUserInfo));
+
+router.patch("/avatar", authenticateToken, upload.single("avatar"), ctrlWrapper(updateAvatar));
+
+router.delete("/avatar", authenticateToken, ctrlWrapper(deleteAvatar));
 
 router.get("/verify/:verificationToken", ctrlWrapper(verifyEmail));
 
